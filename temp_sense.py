ceafd92d4713a072b8618590339acad8660a3883
Redspeed93 <Redspeed93@Protonmail.com>
2018-11-28 09:23:10 +0100

    Added comments

--- temp_sense.py
+++ temp_sense.py
@@ -2,15 +2,25 @@ from sense_hat import SenseHat
 from datetime import *
 import json
 import uuid
+
+# Create an object of the sensehat
 sense = SenseHat()
+
+# Function to create a json string
 
 
 def json_string():
+    # Get temperature, round to 2 decimal points
     temperature = round(sense.get_temperature(), 2)
+    # Get current time
     now = datetime.now()
+    # Stuff everything into a JSON string
+    # Get Mac-address, convert to hexcode, then lastly to string
     json_data = {"mac_address": (str(hex(uuid.getnode())))+"*",
                  "temperature": temperature,
+                 # Format current time to the folllowing format
                  "timestamp": now.strftime("%d-%m-%Y %H:%M:%S"),
                  "indoor": True,
                  "location": "Stue"}
+    # Return JSON object
     return json.dumps(json_data)
--- udp_broadcast.py
+++ udp_broadcast.py
@@ -2,14 +2,18 @@ from socket import *
 import time as t
 from temp_sense import *
 
+# Port to broadcast to
 BROADCAST_TO_PORT = 11912
 
 socket = socket(AF_INET, SOCK_DGRAM)
-# s.bind(('', 14593))     # (ip, port)
-# no explicit bind: will bind to default IP + random port
 socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+# Infinite loop
 while True:
+    # Calls function from temp_sense.py
     data = json_string()
+    # Sends the JSON object as bytes
     socket.sendto(bytes(data, "UTF-8"), ("<broadcast>", BROADCAST_TO_PORT))
+    # Print the JSON object to console
     print(data)
+    # Wait 30 seconds
     t.sleep(30)
