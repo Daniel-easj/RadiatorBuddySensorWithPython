ca2ee77493cf87cd8a20ca74a9ccc7a69fbdc01e
Redspeed93 <Redspeed93@Protonmail.com>
2018-12-07 12:22:25 +0100

    Extracted retrieval of MAC-address to its own function

--- temp_sense.py
+++ temp_sense.py
@@ -11,17 +11,30 @@ sense = SenseHat()
 # Port to broadcast to
 BROADCAST_TO_PORT = 11912
 
+ETHERNET_INTERFACE = 'wlan0'
+
+# Function to get MAC-address
+
+
+def getMAC(interface):
+    # Return the MAC address of the specified interface
+    try:
+        mac_hex_string = open('/sys/class/net/%s/address' % interface).read()
+    except:
+        mac_hex_string = "00:00:00:00:00:00"
+    return mac_hex_string[0:17]
+
 # Function to create a JSON string
 
 
 def json_string():
-    # Get temperature
+        # Get temperature
     temperature = sense.get_temperature()
     # Get current time
     now = datetime.now()
     # Put everything into a JSON string
     # Get Mac-address(Id), convert to hexcode, then lastly to string
-    json_data = {"Id": (str(hex(uuid.getnode()))),
+    json_data = {"Id": getMAC(ETHERNET_INTERFACE),
                  "Temperature": temperature,
                  # Format current time to the folllowing format
                  "Timestamp": now.strftime("%d-%m-%Y %H:%M:%S")}
